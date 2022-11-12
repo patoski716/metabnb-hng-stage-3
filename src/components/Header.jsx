23ddc34632e8388fc23ea1fabe0bc2c3e4067fe6
chiscocode <patrickchukwudifu716@gmail.com>
2022-11-12 07:57:41 +0100

    update header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,6 @@ import Navbar from 'react-bootstrap/Navbar';
 import { Link } from "react-router-dom";
 import Logo from '../images/Vector.png'
 import Home from '../images/Home.png'
-
 import Modal from 'react-bootstrap/Modal';
 import Vector1 from '../images/Vector1.png'
 import Meta from '../images/meta.png'
