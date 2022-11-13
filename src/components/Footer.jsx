597c3cee44677e8d9ba78c6b3fad9300d323adeb
chiscocode <patrickchukwudifu716@gmail.com>
2022-11-13 05:51:00 +0100

    update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,19 +9,21 @@ const Footer = () => {
             <div className="container">
                 <div className="py-5">
                     <div className="row">
-                        <div className="col-md-6 col-lg-3  text-right" style={{marginTop:"20px"}}>
+                        <div className="col-md-6 col-lg-2  text-right" style={{marginTop:"20px"}}>
                             
                                 <img src={Logo} alt="" />
                             <div style={{marginTop:"120px",marginBottom:"100px",fontSize:'7px'}}>
-                                <span className='mx-3 text-white'><i className="fab fa-facebook fa-3x"></i></span>
-                                <span className='mx-3 text-white'><i className="fab fa-instagram fa-3x"></i></span>
-                                <span className='mx-3 text-white'><i className="fab fa-twitter fa-3x"></i></span>
+                                <span className='mx-2 text-white'><i className="fab fa-facebook fa-3x"></i></span>
+                                <span className='mx-2 text-white'><i className="fab fa-instagram fa-3x"></i></span>
+                                <span className='mx-2 text-white'><i className="fab fa-twitter fa-3x"></i></span>
                             </div>
                             <div style={{marginTop:"120px"}}>
                             &copy; 2022 Metabnb
                             </div>
                         </div>
-                        <div className="col-md-6 col-lg-3  text-right" style={{marginTop:"20px"}}>
+                        <div className="col-md-6 col-lg-2  text-right" style={{marginTop:"20px"}}>
+                        </div>
+                        <div className="col-md-6 col-lg-2  text-right" style={{marginTop:"20px"}}>
                             <p style={{fontWidth:'bolder',fontSize:'18px'}}>Community</p>
                             <p style={{fontWidth:'bolder',fontSize:'14px'}}>NFT</p>
                             <p style={{fontWidth:'bolder',fontSize:'14px'}}>Tokens</p>
